8de37f6abc10dafdab4c33b3cbd94b807b05db34
sht0522 <sht0522@sina.com>
2020-11-29 20:15:06 +0800

    config项目

---
+++ configservice/src/main/java/com/project/configservice/ConfigserviceApplication.java
@@ -0,0 +1,13 @@
+package com.project.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserviceApplication.class, args);
+    }
+
+}
